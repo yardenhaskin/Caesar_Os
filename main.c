93fc0b628cf1f05c2aa0aa5c5593c6b5bea0b020
EdwarDaaboul <edwardaaboul@mail.tau.ac.il>
2020-11-13 14:14:46 +0200

    Update main.c

--- main.c
+++ main.c
@@ -7,15 +7,16 @@ main(int argc, char* argv[])
 		printf("Error, Invalid number of arguments\n");
 		return STATUS_CODE_FAILURE;
 	}
-	open_output_file();
+	open_output_file();//Edwar
 	open_input_text()
 	{}
 	find_input_size();
 
+	///Yarden
 	for ()// on the input file
 	{
 		// if a-z
-		decode_small_letters();
+		decode_small_letters(); //outputs char
 		//if A-Z
 		decode_capital_letters();
 		//if 0-9 
@@ -24,7 +25,8 @@ main(int argc, char* argv[])
 		/*do nothing*/
 
 
-		write_decoded_to_output();
+		write_decoded_to_output();//input char
 	}
-	close_files();
+
+	close_files();//Edwar
 }
