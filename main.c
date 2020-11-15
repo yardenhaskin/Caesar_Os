e081b4e6cf3ff7ab9020a495bd131692b1d31b2b
EdwarDaaboul <edwardaaboul@mail.tau.ac.il>
2020-11-15 16:00:11 +0200

    Update main.c

--- main.c
+++ main.c
@@ -9,32 +9,32 @@ const int STATUS_CODE_FAILURE = -1;
 //Step Number One
 main(int argc, char* argv[])
 {
-
-	FILE* filePointer;
+	FILE* filePointer=NULL;
 	char ch;
 
 	if (argc != 3)
-
 	{
 		printf("Error, Invalid number of arguments\n");
 		return STATUS_CODE_FAILURE;
 	}
 
-	open_output_file(argv[1],argv[2]);//Edwar
+	open_output_file(argv[1], argv[2]);//Edwar
 	find_input_file_size(argv[1]);
 
 	while ((ch = fgetc(filePointer)) != EOF) {
-	//for ()// on the input file
-	//{
-		// if a-z
-		//decode_small_letters(); //outputs char
-		//if A-Z
-		//decode_capital_letters();
-		//if 0-9 
-		//decode_number();
-		// other than that
-		/*do nothing*/
-	//	write_decoded_to_output();//input char
+		//for ()// on the input file
 		//{
+			// if a-z
+			//decode_small_letters(); //outputs char
+			//if A-Z
+			//decode_capital_letters();
+			//if 0-9 
+			//decode_number();
+			// other than that
+			/*do nothing*/
+		//	write_decoded_to_output();//input char
+			//{
+		
+	}
 	return 0;
 }
